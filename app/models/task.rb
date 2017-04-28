2a62581b776cb4525feeffb624a73eadadda2b9a
JoelLindow <joellindow@gmail.com>
2017-04-28 13:48:24 -0600

    completed task manager first time

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,15 @@
 require 'sqlite3'
-  attr_reader :title, :description
+
 
 class Task
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
@@ -15,11 +17,20 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
     end
+  end
+
+  def self.find(id)
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
+    Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
   end
 end
