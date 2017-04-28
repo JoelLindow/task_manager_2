bea00f617771acae6a8076849b52c32db619e366
JoelLindow <joellindow@gmail.com>
2017-04-28 12:05:48 -0600

    pushing for inspection. added attr_reader to /app/models/task.rb with errors presenting themselves

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 require 'sqlite3'
+  attr_reader :title, :description
+
 class Task
 
   def initialize(task_params)
@@ -9,6 +11,15 @@ class Task
   end
 
   def save
-  @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+  end
+
+  def self.all
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    tasks = database.execute("SELECT * FROM tasks")
+    tasks.map do |task|
+      Task.new(task)
+    end
   end
 end
