6e6937c8305e7fda4fa6611a4c5ea4bf721717b4
JoelLindow <joellindow@gmail.com>
2017-06-26 11:09:55 -0600

    fixed individual task display

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true  # this allows us to use _method in the form
 
   get '/' do
     erb :dashboard
@@ -22,9 +23,24 @@ class TaskManagerApp < Sinatra::Base
     redirect '/tasks'
   end
 
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
   get '/tasks/:id' do
-  @task = Task.find(params[:id])
-  erb :show
-end
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
 
 end
